23d4bda960a9382c4f44bcbaa934f66c77b2a86a
Sanjeev Kumar Ujjinikoopp <sujjinikoopp@arubanetworks.com>
2019-05-14 11:48:32 +0530

    Removed lock file

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,14 @@ var gulp = require('gulp');
 var wrench = require('wrench');
 var deploy = require('gulp-gh-pages');
 
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
@@ -23,10 +31,3 @@ gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
 
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
